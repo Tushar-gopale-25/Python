4e2a3329fa3ae2b97e4e56588d71a8f71a5051cb
Tushar Gopale <gopaletushar5@gmail.com>
2025-06-14 20:35:25 +0530

    insert_link.py

---
+++ insert_link.py
@@ -0,0 +1,18 @@
+
+def insertAtIndex(self, data, index):
+    if (index == 0):
+        self.insertAtBegin(data)
+        return
+
+    position = 0
+    current_node = self.head
+    while (current_node != None and position+1 != index):
+        position = position+1
+        current_node = current_node.next
+
+    if current_node != None:
+        new_node = Node(data)
+        new_node.next = current_node.next
+        current_node.next = new_node
+    else:
+        print("Index not present")
